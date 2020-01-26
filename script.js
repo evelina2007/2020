439474768a7bf446fa59858d6f77364d13f11688
evelina2007 <60062676+evelina2007@users.noreply.github.com>
2020-01-26 14:07:21 +0300

    punkt2_1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("all ok")
+punkt2_1()
+
+function punkt2_1() {
+  //разница в сравнении
+  let d = {key: "value"};
+  let e = {key: "value"} === d;	 //нестрогое сравнение
+  console.log(e);
+  let a = {key: "value"} === d;  //строгое сравнение
+  console.log(a);
+  let b =d;
+  let c = b === d;        
+  console.log(c);
+}
+
+function punkt2_2() {
+	//экранирование
+	console.log("кино \"аватар\" \n \\n перевод строки")
+}
