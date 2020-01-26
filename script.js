d9aae0663523314d1c36c3251989d2ffa82e78b3
evelina2007 <60062676+evelina2007@users.noreply.github.com>
2020-01-26 14:31:37 +0300

    punkt2_2_1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("all ok")
-punkt2_1()
+punkt2_2()
 
 function punkt2_1() {
   //разница в сравнении
@@ -9,11 +9,26 @@ function punkt2_1() {
   let a = {key: "value"} === d;  //строгое сравнение
   console.log(a);
   let b =d;
-  let c = b === d;        
+  let c = b === d;               //ссылаются  на один и тот жу обьект    
   console.log(c);
 }
 
 function punkt2_2() {
 	//экранирование
-	console.log("кино \"аватар\" \n \\n перевод строки")
+	console.log("кино \"аватар\" \n \\n перевод строки");
+	//функции строк
+	let s = " the Quick bRown FOX quickly jumps over The lazy dog. "
+	console.log(s[1] + s [3])
+	console.log(s.indexOf("e"))      // первое вхождение
+	console.log(s.lastIndexOf("e"))   // последнее вхождение
+	console.log(s.indexOf("ф"))     // если нет такого символа
+	console.log(s.replace("o", "OOO"))
+	let mas = s.split(" ");
+	console.log(mas);
+	console.log("192.168.1.1".split("."))
+	console.log(mas.join("-^-"))
+	console.log(s.substr(5, 10)) 
+	console.log(s.toLowerCase())
+	console.log(s.toUpperCase())
+	console.log(s.trim())
 }
